d58a3c509363b9850b4d3baa915d307871893238
Alvin <huerox@msn.com>
2020-07-27 16:35:43 -0700

    network info.

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -324,7 +324,43 @@ class Main extends Component {
         this.props.fetchComments();
         this.props.fetchPartners();
         this.props.fetchPromotions();
+
+        NetInfo.fetch().then(connectionInfo => {
+            (Platform.OS === 'ios') ?
+                Alert.alert('Initial Network Connectivity Type:', connectionInfo.type) 
+                : ToastAndroid.show('Initial Network Connectivity Type:' +
+                    connectionInfo.type, ToastAndroid.LONG);
+
+        })
+            this.unsubscribeNetinfo = NetInfo.addEventListener(connectionInfo => {
+                this.handleConnectivityChange(connectionInfo);
+            });
     }
+
+    componentWillUnmount() {
+        this.unsubscribeNetinfo();
+    }
+
+    handleConnectivityChange = connectionInfo => {
+        let connectionMsg = 'You are now connected to an active network.';
+        switch (connectionInfo.type) {
+            case 'none':
+                connectionMsg = 'No network connection is active.';
+                break;
+            case 'unknown':
+                connectionMsg = 'The network connection state is now unknown.';
+                break;
+            case 'cellular':
+                connectionMsg = 'You are now connected to a cellular network.';
+                break;
+            case 'wifi':
+                connectionMsg = 'You are now connected to a WiFi network.';
+                break;
+        }
+        (Platform.OS === 'ios') ? Alert.alert('Connection change:', connectionMsg)
+            : ToastAndroid.show(connectionMsg, ToastAndroid.LONG);
+    }
+
 
     render() {
         return (
